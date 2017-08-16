18212fbd4c7d2395f47017224a71caeed9c7272f
Nirali Vasisht <nirali88@gmail.com>
2017-08-16 11:18:59 +0100

    Add restaurant model

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,7 @@ require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
 
-require "jquery"
-require "bootstrap-sprockets"
+
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
